124977d909de128c6c0d28c43260a87ba925e3e1
youyounggyu <dbdudrb012@gmail.com>
2022-06-08 16:44:37 +0900

    four commit

--- project/src/level1/Step3.java
+++ project/src/level1/Step3.java
@@ -3,5 +3,15 @@ package level1;
 public class Step3 {
 	public static void main(String[] args) {
 		
+		solution("");
+	}
+	
+	/**
+	 * 
+	 * @param new_id
+	 */
+	public static void solution(String new_id) {
+		
+		String test = "";
 	}
 }
