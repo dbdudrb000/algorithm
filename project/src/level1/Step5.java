9d575c4916d2b889c79c084c53e9676509e44d98
YouYoungGyu <65213623+dbdudrb000@users.noreply.github.com>
2022-06-21 21:03:04 +0900

    two commit

--- project/src/level1/Step5.java
+++ project/src/level1/Step5.java
@@ -1,13 +1,27 @@
 package level1;
 
 public class Step5 {
-
+    /*
+	[1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]	"right"	 
+	 L  R  L  L  L  R  L  L  R  R  L"
+	
+	[7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]	"left"	 
+	 L  R  L  L  R  R  L  L  L  R  R
+	 
+	[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]	    "right"	 
+	 L  L  R  L  L  R  L  L  R  L
+	*/
+    
 	public static void main(String[] args) {
+		int[] numbers = {};
+		String hand = "";
 		
-		solution();
+		solution(numbers, hand);
 	}
 
-	public static void solution() {
+	public static String solution(int[] numbers, String hand) {
 		
+		
+		return "";
 	}
 }
