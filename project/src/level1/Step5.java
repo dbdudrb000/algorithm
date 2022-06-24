6b2e906bbb16f86765b322b8af9c5e6c0a5ce6d1
YouYoungGyu <65213623+dbdudrb000@users.noreply.github.com>
2022-06-24 23:38:34 +0900

    Level5 Clear

--- project/src/level1/Step5.java
+++ project/src/level1/Step5.java
@@ -13,15 +13,60 @@ public class Step5 {
 	*/
     
 	public static void main(String[] args) {
-		int[] numbers = {};
-		String hand = "";
+		int[] numbers = {7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2};
+		String hand = "left";
 		
-		solution(numbers, hand);
+		System.out.println(solution(numbers, hand));
 	}
 
 	public static String solution(int[] numbers, String hand) {
+		String answer = "";
+		int left = 10;
+		int right = 12;
+		int L_dis
+		   ,R_dis = 0;
 		
+		for(int n : numbers) {
+			if(n == 1 || n == 4 || n == 7) {
+				left = n;
+				answer += "L";
+			} else if(n == 3 || n == 6 || n == 9) {
+				right = n;
+				answer += "R";
+			} else {
+				if(n == 0) {
+					n += 11;
+				}
+				L_dis = (Math.abs(n-left))/3 + (Math.abs(n-left))%3;
+				System.out.println("n : "  + n + "\n left : "  + left);
+				System.out.println("Math.abs(n-left))/3 : " + (Math.abs(n-left))/3);
+				System.out.println("(Math.abs(n-left))%3 : "  + (Math.abs(n-left))%3);
+				System.out.println();
+				
+				R_dis = (Math.abs(right-n))/3 + (Math.abs(right-n))%3;
+				System.out.println("n : "  + n + "\n right : "  + right);
+				System.out.println("(Math.abs(right-n))/3 : " + (Math.abs(right-n))/3);
+				System.out.println("(Math.abs(right-n))%3 : " + (Math.abs(right-n))%3);
+				System.out.println();
+				
+				if(L_dis == R_dis) {
+					if(hand.equals("right")){
+						answer += "R";
+						right = n;
+					} else {
+						answer += "L";
+						left = n;
+					}
+				} else if(L_dis < R_dis) {
+					answer += "L";
+					left = n;
+				} else {
+					answer += "R";
+					right = n;
+				}
+			}
+		}
 		
-		return "";
+		return answer;
 	}
 }
